4535292b40edf7595ecf0e372c53d0464ef01e7c
chathushi24 <cb011177@students.apiit.lk>
2024-10-02 09:00:53 +0530

    update

--- lib/Login.dart
+++ lib/Login.dart
@@ -7,75 +7,169 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        margin: EdgeInsets.symmetric(horizontal:10),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return OrientationBuilder(
+            builder: (context, orientation) {
+              if (orientation == Orientation.portrait) {
+                return _buildPortraitLayout(context);
+              } else {
+                return _buildLandscapeLayout(context);
+              }
+            },
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildPortraitLayout(BuildContext context) {
+    // Portrait layout remains unchanged
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 10),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            height: 300,
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  "Login",
+                  style: TextStyle(
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                _buildEmailField(),
+                _buildPasswordField(),
+                _buildLoginButton(),
+                _buildSignUpRow(context),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildLandscapeLayout(BuildContext context) {
+    return SingleChildScrollView(
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container(
-              height: 300,
-              width: double.infinity,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Text("Login", style: TextStyle(
-                    fontSize: 50,
-                    fontWeight: FontWeight.bold,
-                  ),),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: "Email",
-                      hintStyle: TextStyle(
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: "Password",
-                      suffixIcon: GestureDetector(
-                        child: Icon(Icons.visibility,
-                          color: Colors.black,
-                        ),
-                      ),
-                      hintStyle: TextStyle(
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    height: 45,
-                    width: double.infinity,
-                    child:ElevatedButton(
-                        child:Text("Login"),
-                        // color: Colors.grey,
-                        onPressed:(){}) ,
-                  ),
-                  Row(
+            Text(
+              "Login",
+              style: TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
-                      Text("I don't have an account!"),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      GestureDetector(
-                        onTap: (){
-                          Navigator.pushNamed(context, 'SignUp');
-                        },
-                        child: Text("SignUp", style: TextStyle(
-                          color: Colors.cyan,
-                          fontSize: 15,
-                        ),),
-                      ),
+                      _buildEmailField(isLandscape: true),
+                      SizedBox(height: 20),
+                      _buildPasswordField(isLandscape: true),
                     ],
                   ),
-                ],
-              ),
+                ),
+                SizedBox(width: 20),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      _buildLoginButton(isLandscape: true),
+                      SizedBox(height: 20),
+                      _buildSignUpRow(context),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildEmailField({bool isLandscape = false}) {
+    return TextFormField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        hintText: "Email",
+        hintStyle: TextStyle(
+          color: Colors.black,
+        ),
+        contentPadding: isLandscape
+            ? EdgeInsets.symmetric(vertical: 10, horizontal: 15)
+            : null,
+      ),
+    );
+  }
+
+  Widget _buildPasswordField({bool isLandscape = false}) {
+    return TextFormField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        hintText: "Password",
+        suffixIcon: GestureDetector(
+          child: Icon(
+            Icons.visibility,
+            color: Colors.black,
+          ),
+        ),
+        hintStyle: TextStyle(
+          color: Colors.black,
+        ),
+        contentPadding: isLandscape
+            ? EdgeInsets.symmetric(vertical: 10, horizontal: 15)
+            : null,
+      ),
+    );
+  }
+
+  Widget _buildLoginButton({bool isLandscape = false}) {
+    return Container(
+      height: isLandscape ? 40 : 45,
+      width: isLandscape ? 200 : double.infinity,
+      child: ElevatedButton(
+        child: Text("Login"),
+        onPressed: () {},
+      ),
+    );
+  }
+
+  Widget _buildSignUpRow(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("I don't have an account!"),
+        SizedBox(width: 10),
+        GestureDetector(
+          onTap: () {
+            Navigator.pushNamed(context, 'SignUp');
+          },
+          child: Text(
+            "SignUp",
+            style: TextStyle(
+              color: Colors.cyan,
+              fontSize: 15,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/SignUp.dart
+++ lib/SignUp.dart
@@ -7,114 +7,195 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: SafeArea(
-        child: Form(
-          // key: _formKey,
-          child: Container(
-            child: Column(
-              children: <Widget>[
-                Container(
-                  height: 200,
-                  width: double.infinity,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      Text(
-                        "Register",
-                        style: TextStyle(
-                          fontSize: 50,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
+        child: OrientationBuilder(
+          builder: (context, orientation) {
+            if (orientation == Orientation.portrait) {
+              return _buildPortraitLayout();
+            } else {
+              return _buildLandscapeLayout();
+            }
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPortraitLayout() {
+    return Form(
+      // key: _formKey,
+      child: Container(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 200,
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Text(
+                    "Register",
+                    style: TextStyle(
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  height: 400,
-                  margin: EdgeInsets.symmetric(horizontal: 10),
-                  width: double.infinity,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      TextFormField(
-                        decoration: InputDecoration(
-                          hintText: "Username",
-                          hintStyle: TextStyle(color: Colors.black),
-                          border: OutlineInputBorder(),
-                        ),
-                      ),
-                      TextFormField(
-                        decoration: InputDecoration(
-                          hintText: "Email",
-                          hintStyle: TextStyle(color: Colors.black),
-                          border: OutlineInputBorder(),
-                        ),
-                      ),
-                      TextFormField(
-                        decoration: InputDecoration(
-                          hintText: "Password",
-                          suffixIcon: GestureDetector(
-                            onTap: () {
-                              FocusScope.of(context).unfocus();
-                            },
-                            child: Icon(Icons.visibility, color: Colors.black),
-                          ),
-                          hintStyle: TextStyle(color: Colors.black),
-                          border: OutlineInputBorder(),
-                        ),
-                      ),
-                      TextFormField(
-                        decoration: InputDecoration(
-                          hintText: "Phone number",
-                          hintStyle: TextStyle(color: Colors.black),
-                          border: OutlineInputBorder(),
-                        ),
-                      ),
-                      Container(
-                        height: 50,
-                        width: double.infinity,
-                        child: ElevatedButton(
-                          child: Text("Register"),
-                          onPressed: () {
-                            // validation();
-                          },
-                        ),
-                      ),
-                      Row(
-                        children: [
-                          Text("I already have an account!"),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          GestureDetector(
-                            onTap: () {
-                              Navigator.pushNamed(context, 'Login');
-                            },
-                            child: Text(
-                              "Login",
-                              style: TextStyle(
-                                  color: Colors.blue,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 400,
+              margin: EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  _buildUsernameField(),
+                  _buildEmailField(),
+                  _buildPasswordField(),
+                  _buildPhoneNumberField(),
+                  _buildRegisterButton(),
+                  _buildLoginRow(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLandscapeLayout() {
+    return SingleChildScrollView(
+      child: Form(
+        // key: _formKey,
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: Column(
+                  children: [
+                    Text(
+                      "Register",
+                      style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    _buildRegisterButton(),
+                    SizedBox(height: 20),
+                    _buildLoginRow(),
+                  ],
+                ),
+              ),
+              SizedBox(width: 20),
+              Expanded(
+                flex: 2,
+                child: Column(
+                  children: [
+                    _buildUsernameField(),
+                    SizedBox(height: 10),
+                    _buildEmailField(),
+                    SizedBox(height: 10),
+                    _buildPasswordField(),
+                    SizedBox(height: 10),
+                    _buildPhoneNumberField(),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildUsernameField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        hintText: "Username",
+        hintStyle: TextStyle(color: Colors.black),
+        border: OutlineInputBorder(),
+      ),
+    );
+  }
+
+  Widget _buildEmailField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        hintText: "Email",
+        hintStyle: TextStyle(color: Colors.black),
+        border: OutlineInputBorder(),
+      ),
+    );
+  }
+
+  Widget _buildPasswordField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        hintText: "Password",
+        suffixIcon: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: Icon(Icons.visibility, color: Colors.black),
+        ),
+        hintStyle: TextStyle(color: Colors.black),
+        border: OutlineInputBorder(),
+      ),
+    );
+  }
+
+  Widget _buildPhoneNumberField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        hintText: "Phone number",
+        hintStyle: TextStyle(color: Colors.black),
+        border: OutlineInputBorder(),
+      ),
+    );
+  }
+
+  Widget _buildRegisterButton() {
+    return Container(
+      height: 50,
+      width: double.infinity,
+      child: ElevatedButton(
+        child: Text("Register"),
+        onPressed: () {
+          // validation();
+        },
+      ),
+    );
+  }
+
+  Widget _buildLoginRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("I already have an account!"),
+        SizedBox(width: 10),
+        GestureDetector(
+          onTap: () {
+            Navigator.pushNamed(context, 'Login');
+          },
+          child: Text(
+            "Login",
+            style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ],
     );
   }
 }
